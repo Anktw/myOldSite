a508acbdc9172b0ad9dff446785d41eb8687840b
Anktw <at792226@gmail.com>
2024-09-11 03:38:53 +0530

    fixed projects

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-function showSideBar(){
-    const sidebar=document.querySelector('.sidebar');
-    sidebar.classList.toggle('show-sidebar');
-}
-function hideSideBar(){
-    const sidebar=document.querySelector('.sidebar');
-    sidebar.style.display = 'none';
-}
