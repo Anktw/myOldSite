73dba8a77a01fd5ff73ff994b05b4f10a07f2fd2
Anktw <at792226@gmail.com>
2024-09-12 00:40:01 +0530

    fixed greetings

---
+++ assets/js/greetings.js
@@ -0,0 +1,13 @@
+const greetings = ["Hello", "Ola", "Namaste", "Bonjour", "Hola", "Ciao", "Salut"];
+let currentIndex = 0;
+const greetingElement = document.getElementById('greetings');
+
+function changeGreeting() {
+    greetingElement.style.opacity = 0;
+    setTimeout(() => {
+        currentIndex = (currentIndex + 1) % greetings.length;
+        greetingElement.textContent = greetings[currentIndex];
+        greetingElement.style.opacity = 1;
+    }, 1000); 
+}
+setInterval(changeGreeting, 3000);
