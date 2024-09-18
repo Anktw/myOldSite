a083778430d7dddce0c48215565f7ad66dd003bf
Anktw <at792226@gmail.com>
2024-09-18 23:36:26 +0530

    adding path of learning

---
+++ assets/js/scroll.js
@@ -0,0 +1,14 @@
+window.addEventListener('scroll', function() {
+    var milestones = document.querySelectorAll('.milestone');
+    for (var i = 0; i < milestones.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = milestones[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            milestones[i].classList.add('visible');
+        } else {
+            milestones[i].classList.remove('visible');
+        }
+    }
+});
