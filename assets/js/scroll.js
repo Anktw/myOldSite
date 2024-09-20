90f635875df7eb5f4fce39255a8e03e9043f0ecb
Anktw <at792226@gmail.com>
2024-09-20 17:21:24 +0530

    adding resume

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -13,18 +13,3 @@ window.addEventListener('scroll', function() {
         }
     }
 });
-window.addEventListener('scroll', function() {
-    var milestones = document.querySelectorAll('.vid-list');
-    for (var i = 0; i < milestones.length; i++) {
-        var windowHeight = window.innerHeight;
-        var elementTop = milestones[i].getBoundingClientRect().top;
-        var elementVisible = 150;
-
-        if (elementTop < windowHeight - elementVisible) {
-            milestones[i].classList.add('visible');
-
-        } else {
-            milestones[i].classList.remove('visible');
-        }
-    }
-});
